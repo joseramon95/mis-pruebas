b030599141e5abfe8166f667218b8d91bf5de0d2
unknown <joseramonmadridvaldez@gmail.com>
2025-04-03 20:12:42 -0700

    se movio la coneccion de base de datos a models , se cambio el codigo para mejorarlo , creacion del metodo crear_tarea

--- proyecto railways/models.py
+++ proyecto railways/models.py
@@ -1,16 +1,24 @@
-from database import conectar_bd
+import sqlite3
 
-def agregar_producto(nombre, cantidad):
-    conn = conectar_bd()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO productos (nombre, cantidad) VALUES (?, ?)", (nombre, cantidad))
-    conn.commit()
-    conn.close()
+# se conecta a la base de datos
+def conectar_bd():
 
-def obtener_productos():
+    conn = sqlite3.connect(DATABASE_NAME)
+    return conn
+
+
+def crear_tarea (nombre_usuario, tarea, porcentaje):
     conn = conectar_bd()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM productos")
-    productos = cursor.fetchall()
-    conn.close()
-    return productos
+    try: 
+        cursor = conn.cursor()
+        cursor.execute(
+                "INSERT INTO tareas (id_usuario, nombre_tarea, porcentaje) VALUES (?, ?, ?)",
+                (id_usuario, nombre_tarea, porcentaje) 
+            )
+        conn.commit()
+        return cursor.lastrowid() # da el id de la nueva tarea
+    except exeption as e :
+        conn.rollback() #dezhace cambio si algo sale mal
+        raise e # se lanza alerta para que no deje de funcionar el programa
+    finally:
+        conn.close()
