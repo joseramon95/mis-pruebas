4740e21e94e44d415614a944b27417fbce236821
unknown <joseramonmadridvaldez@gmail.com>
2025-04-04 22:32:40 -0700

    se creo clase gestor tareas

--- proyecto railways/gui.py
+++ proyecto railways/gui.py
@@ -1,4 +1,27 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
-from models import crear_tarea
+import sqlite3
+from models import conectar_bd
 
+
+class GestoTareas:
+    def __init__(self, root):
+        self.root = root
+        try:
+            self.conn = conectar_bd() #manda llamar de models.py
+        except Exception as e :# exepcin por si da error
+            messagebox.showerror("Error", f"no se puede conectar a la BD: {e}")
+            self.root.desroy()
+
+        self.root.title ("Gestor de Tareas Avanzado")
+        self.root.geometry("700x500")
+
+        self.crear_interfaz()
+        self.actualizar_lista()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    from tkinter import simpledialog  # Importación para dialogos modales
+    app = GestorTareas(root)
+    root.mainloop()
